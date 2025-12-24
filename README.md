86c9e892c3e2bbdde356f5abb4f90dc809299bbb
bmalves83 <40076077+bmalves83@users.noreply.github.com>
2025-12-24 21:16:30 +0000

    configurado o `prettier`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para estudo do https://curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Olha só meu bem, publiquei essa página num ambiente de cloud! Feliz aniversário 🎉🎉🎉 Te amo!!!</h1>   
+  return (
+    <h1>
+      Olha só meu bem, publiquei essa página num ambiente de cloud! Feliz
+      aniversário 🎉🎉🎉 Te amo!!!
+    </h1>
+  );
 }
 
 export default Home;
