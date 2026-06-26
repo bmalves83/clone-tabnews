6864e4fd6deb229c503b6e9a88ce23553c7b1996
bmalves83 <40076077+bmalves83@users.noreply.github.com>
2026-06-26 02:33:37 +0000

    add `.env` e `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function showStatus(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as soma;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Teste de resposta om acentos ção" });
 }
 
-export default showStatus;
+export default status;
