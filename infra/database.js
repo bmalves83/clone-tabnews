b292f0652dc274d452c94f0f00607ecc352b7dc2
bmalves83 <40076077+bmalves83@users.noreply.github.com>
2026-07-01 01:53:24 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,25 @@
+// infra/database.js (NÃO RECOMENDADO para uso geral em APIs)
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = await new Client({
+  const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  // let result; // Declare result fora do bloco try
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject); // Atribua a result
+    return result; // Agora result está acessível aqui
+  } catch (err) {
+    console.error("Erro na consulta:", err);
+    throw err; // Re-lança o erro para que o chamador possa tratá-lo
+  } finally {
+    await client.end(); // Garante que a conexão seja fechada
+  }
 }
 
 export default {
