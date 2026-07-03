85339334cf46a4b1653d500706761cb1cc4f9697
bmalves83 <40076077+bmalves83@users.noreply.github.com>
2026-07-03 16:17:44 +0000

    add robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true, // Habilita SSL para conexões seguras
-  });
-  console.log("Credencias DB: ", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(), // Habilita SSL para conexões seguras
   });
   // let result; // Declare result fora do bloco try
   try {
@@ -33,3 +26,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
